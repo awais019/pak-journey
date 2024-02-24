0b5e3f6f1073b3cd134955449579165dd715a4ec
Hamza7765 <chhami461@gmail.com>
2024-02-24 15:51:39 +0500

    authjs/resetpassword

---
+++ pages/resetpassword.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex h-screen items-center justify-center bg-[#EAE4F6]">
+    <div class="h-auto w-[539px] rounded-2xl bg-white text-[#393C3E]">
+      <div class="mx-8">
+        <div class="mt-20 text-center">
+          <p class="text-4xl font-normal">Reset password</p>
+        </div>
+        <div class="mt-10 grid space-y-6 text-[#7C7C7C]">
+          <div>
+            <p class="mb-2">Email address</p>
+            <input
+              class="h-[46px] w-full rounded-md border-[1px] border-[#CFCFCF] px-4"
+              id="name"
+            />
+          </div>
+        </div>
+        <div class="mb-20 mt-5 flex justify-center">
+          <button class="h-12 w-44 rounded-lg bg-[#645BBB] text-white">
+            Reset password
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
